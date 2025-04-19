30304007ca1d6552f4c75a9a630babe75060b7fc
lewis0811 <105399564+lewis0811@users.noreply.github.com>
2025-04-20 02:45:21 +0700

    Implement: Modified Cart item attribute
    Implement ProductVariant (optional)

---
+++ RookieEcommerce.Infrastructure/Migrations/20250419190157_ModifiedCartItem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RookieEcommerce.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifiedCartItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProductVariantId",
+                table: "CartItems",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_ProductVariantId",
+                table: "CartItems",
+                column: "ProductVariantId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_ProductVariants_ProductVariantId",
+                table: "CartItems",
+                column: "ProductVariantId",
+                principalTable: "ProductVariants",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_ProductVariants_ProductVariantId",
+                table: "CartItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItems_ProductVariantId",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "ProductVariantId",
+                table: "CartItems");
+        }
+    }
+}
