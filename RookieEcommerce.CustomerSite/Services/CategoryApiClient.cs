cdb0b314dca1573f62a11d8e6950f737eb11cade
lewis0811 <105399564+lewis0811@users.noreply.github.com>
2025-04-22 20:56:52 +0700

    feat: Add entity http clients
    Add Category, Product Api Client

---
+++ RookieEcommerce.CustomerSite/Services/CategoryApiClient.cs
@@ -0,0 +1,16 @@
+﻿using RookieEcommerce.SharedViewModels.CategoryDtos;
+using RookieEcommerce.SharedViewModels.ProductDtos;
+using RookieEcommerce.SharedViewModels.ResponseDtos;
+
+namespace RookieEcommerce.CustomerSite.Services
+{
+    public class CategoryApiClient(HttpClient httpClient)
+    {
+        public async Task<PaginationResponseDto<CategoryDetailsDto>> GetCategoriesAsync()
+        {
+            var categories = await httpClient
+                .GetFromJsonAsync<PaginationResponseDto<CategoryDetailsDto>>("api/v1/categories?IsIncludeItems=true");
+            return categories!;
+        }
+    }
+}
---
+++ RookieEcommerce.CustomerSite/Services/ProductApiClient.cs
@@ -0,0 +1,15 @@
+﻿using RookieEcommerce.SharedViewModels.ProductDtos;
+using RookieEcommerce.SharedViewModels.ResponseDtos;
+
+namespace RookieEcommerce.CustomerSite.Services
+{
+    public class ProductApiClient(HttpClient httpClient)
+    {
+        public async Task<PaginationResponseDto<ProductDetailsDto>> GetProductsAsync()
+        {
+            var products = await httpClient
+                .GetFromJsonAsync<PaginationResponseDto<ProductDetailsDto>>("api/v1/products");
+            return products!;
+        }
+    }
+}
