1228e2c84c91567aa767b4297060a7226f861c0c
lewis0811 <105399564+lewis0811@users.noreply.github.com>
2025-04-22 16:04:14 +0700

    feat: Implement ApplicationRole

---
+++ RookieEcommerce.Api/Constants/ApplicationRole.cs
@@ -0,0 +1,8 @@
+﻿namespace RookieEcommerce.Api.Constants
+{
+    public static class ApplicationRole
+    {
+        public static readonly string Admin = "ADMIN";
+        public static readonly string User = "USER";
+    }
+}
