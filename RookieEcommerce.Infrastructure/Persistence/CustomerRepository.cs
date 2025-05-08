e56d56536e84c49ca86d353769cc5dc7f3bbd758
lewis0811 <105399564+lewis0811@users.noreply.github.com>
2025-05-08 09:07:56 +0700

    feat: Update customer repository
    - Implement searching, sorting method

--- RookieEcommerce.Infrastructure/Persistence/CustomerRepository.cs
+++ RookieEcommerce.Infrastructure/Persistence/CustomerRepository.cs
@@ -3,9 +3,10 @@ using Microsoft.EntityFrameworkCore.Query;
 using RookieEcommerce.Application.Common;
 using RookieEcommerce.Application.Contacts.Persistence;
 using RookieEcommerce.Application.Features.Customers.Queries;
+using RookieEcommerce.Application.Features.Products.Queries;
 using RookieEcommerce.Domain.Entities;
 using System.Linq.Expressions;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using System.Linq.Dynamic.Core;
 
 namespace RookieEcommerce.Infrastructure.Persistence
 {
@@ -29,11 +30,39 @@ namespace RookieEcommerce.Infrastructure.Persistence
             return await query.FirstOrDefaultAsync(c => c.Id == id.ToString(), cancellationToken);
         }
 
-        public Task<PaginationList<Customer>> GetPaginated(GetCustomersQuery request)
+        public Task<PaginationList<Customer>> GetPaginated(GetCustomersQuery query)
         {
             var customers = context.Customers.AsQueryable();
 
-            return Task.FromResult(PaginationList<Customer>.Create(customers, request.PageSize, request.PageNumber));
+            customers = ApplySearch(query, customers);
+
+            // Apply sorting if sort by is not null
+            customers = ApplySort(query, customers);
+
+            return Task.FromResult(PaginationList<Customer>.Create(customers, query.PageSize, query.PageNumber));
+        }
+
+        private static IQueryable<Customer> ApplySort(GetCustomersQuery query, IQueryable<Customer> customers)
+        {
+            if (!string.IsNullOrEmpty(query.SortBy))
+            {
+                customers = customers.OrderBy(query.SortBy);
+            }
+            return customers;
+        }
+
+        private static IQueryable<Customer> ApplySearch(GetCustomersQuery query, IQueryable<Customer> customers)
+        {
+            if (!string.IsNullOrWhiteSpace(query.SearchTerm))
+            {
+                customers = customers.Where(c => c.Email!.Contains(query.SearchTerm) ||
+                                               c.UserName!.Contains(query.SearchTerm) ||
+                                               c.FirstName!.Contains(query.SearchTerm) ||
+                                               c.LastName!.Contains(query.SearchTerm)
+                                               );
+            }
+
+            return customers;
         }
     }
 }
