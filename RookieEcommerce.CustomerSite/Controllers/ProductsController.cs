3b02cc0ab0e755ee1c22e01a22d0a459a76dab71
lewis0811 <105399564+lewis0811@users.noreply.github.com>
2025-05-08 09:22:07 +0700

    feat: Add logic check for rating, show sku, quantity
    - ProductsController: Add authentication check
    - Details:
     + Add logic to check if user is logged in and is ordered the product before rating
     + Fixing UI

--- RookieEcommerce.CustomerSite/Controllers/ProductsController.cs
+++ RookieEcommerce.CustomerSite/Controllers/ProductsController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using OpenIddict.Client.AspNetCore;
 using RookieEcommerce.CustomerSite.Models;
 using RookieEcommerce.CustomerSite.Services;
 using RookieEcommerce.SharedViewModels.CartDtos;
+using RookieEcommerce.SharedViewModels.OrderDtos;
 using RookieEcommerce.SharedViewModels.ProductRatingDtos;
+using static OpenIddict.Abstractions.OpenIddictConstants;
 
 namespace RookieEcommerce.CustomerSite.Controllers
 {
@@ -24,30 +28,42 @@ namespace RookieEcommerce.CustomerSite.Controllers
             {
                 return BadRequest(ModelState);
             }
+            var token = await HttpContext.GetTokenAsync(OpenIddictClientAspNetCoreConstants.Tokens.BackchannelAccessToken);
 
-            var product = await productApiClient.GetProductByIdAsync(productId)
+            bool userIsLoggedIn = User.Identity!.IsAuthenticated;
+            var customerId = User.Claims.FirstOrDefault(c => c.Type == Claims.Subject)?.Value;
+            ViewBag.UserIsLoggedIn = userIsLoggedIn;
+            ViewBag.CustomerId = customerId;
+
+            OrderDetailsDto? order = new();
+            var product = await productApiClient.GetProductByIdAsync(productId, token)
                 ?? throw new InvalidOperationException($"Product Id {productId} not found.");
-            var rating = await productRatingApiClient.GetProductRatingsAsync(productId)
+
+            var rating = await productRatingApiClient.GetProductRatingsAsync(productId, token)
                 ?? throw new InvalidOperationException($"Rating for product Id {productId} not found.");
 
-            var customerId = Guid.Parse("4C1E0C92-0BEA-A47A-6C8A-59E397F632F2"); // Change to get from cookie later
-            var order = await orderApiClient.GetOrderItemAsync(customerId)
-                ?? throw new InvalidOperationException($"Order for customer Id {customerId}"); // Change to get customerId from cookie later // Please check if customer have order please Lewis
+
+            if (customerId != null)
+            {
+                order = await orderApiClient.GetOrderDetailAsync(Guid.Parse(customerId));
+            }
 
             return View(new HomeProductDetailsViewModel { ProductDetails = product, ProductRatings = rating, OrderDetails = order });
         }
 
         [HttpPost]
-        public async Task<IActionResult> Rating(CreateProductRatingDto dto)
+        public async Task<IActionResult> Rating(CreateProductRatingDto createProductRating)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
+            var token = await HttpContext.GetTokenAsync(OpenIddictClientAspNetCoreConstants.Tokens.BackchannelAccessToken);
+            if (token == null) { RedirectToAction("Login", "Authentication"); }
 
-            await productRatingApiClient.AddProductRatingAsync(dto);
+            await productRatingApiClient.AddProductRatingAsync(createProductRating, token);
 
-            return RedirectToAction("Details", "Products", new { productId = dto.ProductId });
+            return RedirectToAction("Details", "Products", new { productId = createProductRating.ProductId });
         }
 
         [HttpPost]
@@ -57,16 +73,23 @@ namespace RookieEcommerce.CustomerSite.Controllers
             {
                 return BadRequest(ModelState);
             }
+            var token = await HttpContext.GetTokenAsync(OpenIddictClientAspNetCoreConstants.Tokens.BackchannelAccessToken);
+            if (token == null) { RedirectToAction("Login", "Authentication"); }
 
-            var customerId = Guid.Parse("4C1E0C92-0BEA-A47A-6C8A-59E397F632F2"); // Change to get from cookie later
-            var cart = await cartApiClient.GetCustomerCartAsync(customerId);
-            Guid cartId;
+            Guid cartId = Guid.Empty;
+            var customerId = User.Claims.FirstOrDefault(c => c.Type == Claims.Subject)?.Value;
 
-            if (cart == null)
+            if (customerId != null)
             {
-                cartId = (Guid)await cartApiClient.CreateCustomerCartAsync(customerId);
+                var cart = await cartApiClient.GetCustomerCartAsync(Guid.Parse(customerId), token);
+
+                if (cart == null)
+                {
+                    cartId = (Guid)await cartApiClient.CreateCustomerCartAsync(Guid.Parse(customerId));
+                }
+                else { cartId = cart.Id; }
             }
-            else { cartId = cart.Id; }
+
             await cartItemApiClient.AddCartItemAsync(cartItem, cartId);
 
             return RedirectToAction("Index", "Cart");
